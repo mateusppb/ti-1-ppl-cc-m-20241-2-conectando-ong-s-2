fd0a481a2e58393c9bd8f1d9f127c60cf635b3e9
GiovanaFioravante <166036827+GiovanaFioravante@users.noreply.github.com>
2024-06-12 19:57:05 -0300

    Delete cadLog directory

--- cadLog/logout.js
+++
@@ -1,8 +0,0 @@
-function logout() {
-    // Remover os dados do usuário atual do localStorage
-    localStorage.removeItem("name");
-    localStorage.removeItem("email");
-
-    // Redirecionar para a página de login
-    window.location.href = "../inicial/index.html";
-}
