0cab30501b9f43340377d79e5adec883f965e06a
Lorena Simões Salles <166035633+Lorenassalles@users.noreply.github.com>
2024-06-27 17:05:55 -0300

    Update Lorena.js

--- Integração/Trabalho TI pagina ONGs/Lorena.js
+++ Integração/Trabalho TI pagina ONGs/Lorena.js
@@ -21,7 +21,7 @@ function leDados() {
                         "telefone": "(31) 98457-7640",
                         "whatsapp": "https://wa.me/553184577640",
                         "instagram": "@ame_um_animal_ONG",
-                        "email": "mailto:email@provedor.com.br"
+                        "email": "email@provedor.com.br"
                     },
                     "o_que_doar": ["Ração", "Cobertores", "Brinquedos"],
                     "outras_formas_de_doar": "#"
@@ -37,7 +37,7 @@ function leDados() {
                         "telefone": "(11) 98765-4321",
                         "whatsapp": "https://wa.me/5511987654321",
                         "instagram": "@Doação_Amiga",
-                        "email": "mailto:contato@doacaoamiga.com.br"
+                        "email": "contato@doacaoamiga.com.br"
                     },
                     "o_que_doar": ["Roupas", "Alimentos não perecíveis", "Produtos de higiene"],
                     "outras_formas_de_doar": "#"
@@ -53,7 +53,7 @@ function leDados() {
                         "telefone": "(21) 91234-5678",
                         "whatsapp": "https://wa.me/5521912345678",
                         "instagram": "@amigos_dos_animais_ong",
-                        "email": "mailto:contato@amigosdosanimais.org.br"
+                        "email": "contato@amigosdosanimais.org.br"
                     },
                     "o_que_doar": ["Alimentos enlatados para pets", "Caminhas", "Produtos de limpeza"],
                     "outras_formas_de_doar": "#"
@@ -64,28 +64,63 @@ function leDados() {
     return objDados;
 }
 
-function salvaComentarios(comentarios) {
-    localStorage.setItem('comentarios', JSON.stringify(comentarios));
+function imprimeDados() {
+    const logoDaOng = document.querySelector(".Logo img");
+    const nomeDaOng = document.querySelector(".sobre h1");
+    const descricaoDaOng = document.querySelector(".sobre p");
+    const localizaçãoDaOng = document.querySelector(".L li");
+    const horarioOng = document.querySelector(".H li ");
+    const telefoneDaOng = document.querySelector(".telefone");
+    const whatsappDaOng = document.querySelector(".whatsapp");
+    const instaDaOng = document.querySelector(".insta");
+    const emailDaOng = document.querySelector(".email");
+    const oqDoar = document.querySelector(".doacoes ul");
+
+    let objDados = leDados();
+    const ong = objDados.ONGS.find(ong => ong.id == id);
+
+    nomeDaOng.textContent = ong.nome;
+    descricaoDaOng.textContent = ong.sobre;
+    logoDaOng.setAttribute("src", ong.logo);
+    horarioOng.innerHTML += ong.horário;
+    localizaçãoDaOng.innerHTML += ong.localização;
+    telefoneDaOng.textContent = ong.contato.telefone;
+    whatsappDaOng.setAttribute("href", ong.contato.whatsapp);
+    instaDaOng.setAttribute("href", ong.contato.instagram);
+    emailDaOng.setAttribute("href", ong.contato.email);
+    ong.o_que_doar.forEach(item => { oqDoar.innerHTML += `<li><p>${item}</p></li>` });
 }
 
-function leComentarios() {
+// Funções de comentários
+function salvaComentarios(comentarios) {
+    let allComments = leTodosComentarios();
+    allComments[id] = comentarios;
+    localStorage.setItem('comentarios', JSON.stringify(allComments));
+}
+
+function leTodosComentarios() {
     let strComentarios = localStorage.getItem('comentarios');
-    let objComentarios = [];
+    let objComentarios = {};
     if (strComentarios) {
         objComentarios = JSON.parse(strComentarios);
     }
     return objComentarios;
 }
 
-function adicionaComentario(nome, comentario) {
+function leComentarios() {
+    let allComments = leTodosComentarios();
+    return allComments[id] || [];
+}
+
+function adicionaComentario(email, comentario) {
     const comentarios = leComentarios();
-    comentarios.push({ nome, comentario, id: Date.now() });
+    comentarios.push({ email, comentario, id: Date.now() });
     salvaComentarios(comentarios);
 }
 
-function apagaComentario(id) {
+function apagaComentario(comentarioId) {
     let comentarios = leComentarios();
-    comentarios = comentarios.filter(comentario => comentario.id !== id);
+    comentarios = comentarios.filter(comentario => comentario.id !== comentarioId);
     salvaComentarios(comentarios);
     imprimeComentarios();
 }
@@ -94,41 +129,20 @@ function imprimeComentarios() {
     const listaComentarios = document.getElementById('listaComentarios');
     listaComentarios.innerHTML = '';
     const comentarios = leComentarios();
+    const currentUserEmail = localStorage.getItem('currentUserEmail');
 
-    comentarios.forEach(({ nome, comentario, id }) => {
+    comentarios.forEach(({ email, comentario, id }) => {
         const novoComentario = document.createElement('li');
         novoComentario.classList.add('list-group-item');
-        novoComentario.innerHTML = `<strong>${nome}:</strong> ${comentario}<span class="btn-apagar" onclick="apagaComentario(${id})">Apagar</span>`;
+        
+        if (email === currentUserEmail) {
+            novoComentario.innerHTML = `<strong>${email}:</strong> ${comentario}<span class="btn-apagar" onclick="apagaComentario(${id})"> Apagar</span>`;
+        } else {
+            novoComentario.innerHTML = `<strong>${email}:</strong> ${comentario}`;
+        }
+        
         listaComentarios.appendChild(novoComentario);
     });
-}
-
-function imprimeDados() {
-    const logoDaOng = document.querySelector(".Logo img")
-    const nomeDaOng = document.querySelector(".sobre h1")
-    const descricaoDaOng = document.querySelector(".sobre p")
-    const localizaçãoDaOng = document.querySelector(".L li")
-    const horarioOng = document.querySelector(".H li ")
-    const telefoneDaOng = document.querySelector(".telefone")
-    const whatsappDaOng = document.querySelector(".whatsapp")
-    const instaDaOng = document.querySelector(".insta")
-    const emailDaOng = document.querySelector(".email")
-    const oqDoar = document.querySelector(".doacoes ul")
-
-    let objDados = leDados()
-    const ong = objDados.ONGS.find(ong => ong.id == id)
-
-    console.log(ong)
-    nomeDaOng.textContent = ong.nome
-    descricaoDaOng.textContent = ong.sobre
-    logoDaOng.setAttribute("src", ong.logo)
-    horarioOng.innerHTML += ong.horário
-    localizaçãoDaOng.innerHTML += ong.localização
-    telefoneDaOng.textContent = ong.contato.telefone
-    whatsappDaOng.setAttribute("href", ong.contato.whatsapp)
-    instaDaOng.setAttribute("href", ong.contato.instagram)
-    emailDaOng.setAttribute("href", ong.contato.email)
-    ong.o_que_doar.forEach(item => { oqDoar.innerHTML += `<li><p>${item}</p></li>` })
 }
 
 // Adiciona evento de carregamento da página para exibir os dados da ONG
@@ -141,15 +155,28 @@ window.addEventListener('load', () => {
 document.getElementById('comentarioForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
     const comentario = document.getElementById('comentario').value;
 
-    if (nome && comentario) {
-        adicionaComentario(nome, comentario);
-        imprimeComentarios();
-
-        // Limpar os campos do formulário
-        document.getElementById('nome').value = '';
-        document.getElementById('comentario').value = '';
+    if (email && comentario) {
+        // Verificar se o email está cadastrado
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        const userExists = users.some(user => user.email === email);
+        
+        if (userExists) {
+            adicionaComentario(email, comentario);
+            imprimeComentarios();
+            
+            // Limpar os campos do formulário
+            document.getElementById('email').value = '';
+            document.getElementById('comentario').value = '';
+        } else {
+            alert('Email não cadastrado. Por favor, cadastre-se ou insira um email válido.');
+        }
+    } else {
+        alert('Por favor, preencha todos os campos.');
     }
 });
+;
+
+
